e5c1ab296ef10b82a14da74df7d3b34c22e674d5
Scott Kellen <scott@mac.lan>
2024-12-12 22:16:17 -0600

    Final Code Clean

--- Kellen_Final_Project.py
+++ Kellen_Final_Project.py
@@ -5,9 +5,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-# from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
 
 # Read in data
 s = pd.read_csv("social_media_usage.csv")
